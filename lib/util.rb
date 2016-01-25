0e973e96894a11288bddac62f6802bcfe096eeee
Dave Yarwood <dave.yarwood@gmail.com>
2016-01-25 11:01:43 -0500

    reorganizing a bit

--- lib/util.rb
+++ lib/util.rb
@@ -1,5 +1,33 @@
-class String
+module Util
+  # Returns a percentage in the range of plus or minus `p`, where the value is
+  # more likely to be closer to 0, as in a pyramid-shaped distribution.
+  #
+  # e.g. `plus_or_minus(25)` will return a value between -0.25 and 0.25, likely
+  # closer to 0.
+  def plus_or_minus(p)  # +/- "n" percent (50, 25, etc.)
+    q = 1
+    lower = 1
+    upper = 1
+    table = Hash.new
 
+    (-p..p).to_a.each do |prob|
+      upper = lower + (q - 1)
+
+      (lower..upper).to_a.each do |k|
+        table[k] = prob
+      end
+
+      q += 1 if prob < 0
+      q -= 1 if prob >= 0
+
+      lower = upper + 1
+    end
+
+    table[rand(1..upper)].to_f / 100
+  end
+end
+
+class String
   # Divides a string up into n-character lines.  Pushes a word down onto the
   # next line if it doesn't fit, like on a page in a book.
   def line_divide(n)
@@ -32,6 +60,5 @@ class String
   def eighty
     self.line_divide(80)
   end
-
 end
 
